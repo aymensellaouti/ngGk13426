9fbaa7360a1da1993750b1617fb76882b3bce7e1
aymensellaouti <aymen.sellaouti@gmail.com>
2026-04-13 16:39:56 +0100

    corretion color Component

---
+++ src/app/components/color/color.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-color',
+  templateUrl: './color.component.html',
+  styleUrls: ['./color.component.css'],
+})
+export class ColorComponent {
+  #defaultColor = 'red';
+  // state de mon composant
+  /**
+   * @var représente la couleur du background
+   */
+  color = this.#defaultColor;
+
+  //comportement
+
+  /**
+   * permet de changer la couleur du background
+   * @param colorInput : L'input qui représente la couleur
+   */
+  changeColor(colorInput: HTMLInputElement) {
+    this.color = colorInput.value;
+    colorInput.value = '';
+  }
+  /**
+   * remet la couleur à jour avec la valeur par défaut
+   */
+  reset() {
+    this.color = this.#defaultColor;
+  }
+}
