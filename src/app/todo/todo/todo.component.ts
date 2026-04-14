70e2316ccf1098d0f8adb949c294a8fb09c60925
aymensellaouti <aymen.sellaouti@gmail.com>
2026-04-14 16:15:47 +0100

    correction gestion des todo

--- src/app/todo/todo/todo.component.ts
+++ src/app/todo/todo/todo.component.ts
@@ -1,11 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { TodoService } from "../service/todo.service";
+import { Todo } from "../model/todo";
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css'],
-
 })
 export class TodoComponent {
+  todoService = inject(TodoService);
+  /**
+   * La liste des todos à afficher
+   */
+  todos = this.todoService.getTodos();
+  /**
+   * Le todo à ajouter
+   */
+  todo = new Todo();
   constructor() {}
+  addTodo() {
+    this.todoService.addTodo(this.todo);
+    this.todo = new Todo();
+  }
+  delete(todo: Todo) {
+    this.todoService.deleteTodo(todo);
+  }
 }
