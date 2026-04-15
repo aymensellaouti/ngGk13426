77a7f9934d02478f0a3030a531781a99eb13467b
aymensellaouti <aymen.sellaouti@gmail.com>
2026-04-15 10:28:08 +0100

    ajout detailsCvComponent squelette

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,6 +32,7 @@ import { TodoComponent } from './todo/todo/todo.component';
 import { WeekTodoComponent } from './todo/week-todo/week-todo.component';
 import { EmbaucheComponent } from './cv/embauche/embauche.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { DetailsCvComponent } from './cv/details-cv/details-cv.component';
 
 @NgModule({
   declarations: [
@@ -48,6 +49,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     CvItemComponent,
     CvCardComponent,
     EmbaucheComponent,
+    DetailsCvComponent,
     NgStyleComponent,
     MiniWordComponent,
     NgClassComponent,
