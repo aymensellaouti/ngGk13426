ba6f2980bbfd1b7b5cf0f6f7c33c7e139c62e9cd
aymensellaouti <aymen.sellaouti@gmail.com>
2026-04-15 10:25:51 +0100

    exemple route dynamique, utilisation desservices router et activatedRoute Add SecondComponent and update routing; enhance navbar links with active styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ import { CvComponent } from './cv/cv/cv.component';
 import { TodoComponent } from './todo/todo/todo.component';
 import { MiniWordComponent } from './directives/mini-word/mini-word.component';
 import { ColorComponent } from './components/color/color.component';
+import { SecondComponent } from './components/second/second.component';
+
+// /cv
 
 const routes: Routes = [
   // Si le user demande l'uri '/', c'est qu'il veut afficher le FirstComponent
@@ -13,6 +16,8 @@ const routes: Routes = [
   { path: 'todo', component: TodoComponent},
   { path: 'word', component: MiniWordComponent},
   { path: 'color', component: ColorComponent},
+  { path: 'hello/:name', component: SecondComponent},
+
 ];
 
 @NgModule({
--- src/app/components/second/second.component.ts
+++ src/app/components/second/second.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-second',
   template: `
     <div class="alert alert-dark">
-      Je suis le second Component
+      Hello {{name}}
       <p>test color</p>
     </div>
   `,
   styles: [``]
 })
 export class SecondComponent {
-
+  name = 'anonyme';
+  blacklist = ['aymen'];
+  acr = inject(ActivatedRoute);
+  router = inject(Router);
+  constructor() {
+    console.log(this.acr);
+    this.name = this.acr.snapshot.params['name'];
+    if (this.blacklist.includes(this.name)) {
+      this.router.navigate(['']);
+    }
+  }
 }
