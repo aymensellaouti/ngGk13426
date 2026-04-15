c3c9b59d30a3a2fb51a46e4d06544b185b14c7b5
aymensellaouti <aymen.sellaouti@gmail.com>
2026-04-15 10:01:04 +0100

    mise en place du routeur

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './components/first/first.component';
+import { CvComponent } from './cv/cv/cv.component';
+import { TodoComponent } from './todo/todo/todo.component';
+import { MiniWordComponent } from './directives/mini-word/mini-word.component';
+import { ColorComponent } from './components/color/color.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Si le user demande l'uri '/', c'est qu'il veut afficher le FirstComponent
+  { path: '', component: FirstComponent},
+  { path: 'cv', component: CvComponent},
+  { path: 'todo', component: TodoComponent},
+  { path: 'word', component: MiniWordComponent},
+  { path: 'color', component: ColorComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
