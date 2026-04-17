4d3fa9f7cf22cf5ccde2b690fc49e1867d84f474
aymensellaouti <aymen.sellaouti@gmail.com>
2026-04-17 14:31:26 +0100

    correction exercice TTC

--- src/app/signals/ttc/ttc.component.ts
+++ src/app/signals/ttc/ttc.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ttc',
   templateUrl: './ttc.component.html',
-  styleUrl: './ttc.component.css'
+  styleUrl: './ttc.component.css',
+  imports: [FormsModule, CurrencyPipe]
 })
 export class TtcComponent {
+  // Notre state
+  prixHt = signal(0);
+  taxe = signal(18);
+  qty = signal(1);
 
+  unitaireTtc = computed(() => this.prixHt() * (100+this.taxe()) / 100);
+  discount = computed(() => {
+    let discountPercentage = 0;
+    if (this.qty() >= 10 && this.qty() <= 15) {
+      discountPercentage = 20;
+    } else if (this.qty() > 15) {
+      discountPercentage = 20;
+    }
+    return (this.unitaireTtc() * discountPercentage / 100);
+  })
+  totalTtc = computed(() => (this.unitaireTtc() - this.discount()) * this.qty())
 }
