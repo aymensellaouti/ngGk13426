74c8cc2ddfa5cf09a4b7559c60ffd10dfd8f2c75
aymensellaouti <aymen.sellaouti@gmail.com>
2026-04-17 11:15:22 +0100

    standalone bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    standalone: false
+    imports: [NavbarComponent, RouterOutlet]
 })
 export class AppComponent {
   title = 'ngGk13426';
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,28 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { LoggerService } from './app/services/logger.service';
+import { HelloService } from './app/services/hello.service';
+import { AuthInterceptorProvider } from './app/auth/interceptors/auth.interceptor';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { AppRoutingModule } from './app/app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule, FormsModule, ToastrModule.forRoot(), ReactiveFormsModule),
+        // Voila ce que tu devra fournir (Notre menu)
+        LoggerService,
+        HelloService,
+        AuthInterceptorProvider,
+        provideHttpClient(withInterceptorsFromDi()),
+        provideAnimations()
+    ]
+})
   .catch(err => console.error(err));
